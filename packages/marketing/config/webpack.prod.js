18a9a01a14b18ffde65f7749f0e7fd9b983d8464
Sacha Morgese <sachamorgese@gmail.com>
2021-03-13 10:32:07 +0000

    Added webpack configuration

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -3,8 +3,6 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
 
-const domain = process.env.PRODUCTION_DOMAIN;
-
 const prodConfig = {
   mode: 'production',
   output: {
@@ -12,9 +10,10 @@ const prodConfig = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap'
       },
       shared: packageJson.dependencies,
     })
